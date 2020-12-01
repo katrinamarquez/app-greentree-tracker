c6dea850a80e705549c0bc53af4af2dcab658224
katrinamarquez <katrina.l.marquez@gmail.com>
2020-12-01 15:39:38 +1000

    update app.js

--- app.js
+++ app.js
@@ -2,12 +2,31 @@ const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const plantRouter = require("./routes/plant_routes.js")
+const mongoose = require("mongoose")
 
 const port = 3000
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
+
+const dbConn = "mongodb://localhost/app-greentree-tracker"
+
+mongoose.connect(
+  dbConn,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  },
+  err => {
+    if (err) {
+      console.log("Error connecting to database", err)
+    } else {
+      console.log("Connected to database!")
+    }
+  }
+)
 
 app.use("/plants", plantRouter)
 
