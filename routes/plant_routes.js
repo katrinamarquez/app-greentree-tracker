931cf1e1f26a012f9d8f3ee00f065398fb3f95cf
katrinamarquez <katrina.l.marquez@gmail.com>
2020-12-01 11:56:23 +1000

    save b4 lunch

--- routes/plant_routes.js
+++ routes/plant_routes.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const router = express.Router()
+const { getPlants, getPlant } = require("../controllers/plants_controller")
 
 // READ
 // GET on '/plants'
 // Returns all plants
+router.get("/", getPlants)
 
 // READ
 // GET on '/plants/:id'
 // Returns plant with given id
+router.get("/", getPlant)
 
 // CREATE
 // POST on '/plants'
