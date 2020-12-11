3bb264bfdd99d2c19e527fc5dc57f44a8a8ae6c1
katrinamarquez <katrina.l.marquez@gmail.com>
2020-12-11 15:05:45 +1000

    user model done

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+// Required fields for user info
+const User = new Schema ({
+  email: {
+    type: String,
+    required: true
+},
+password: {
+    type: String,
+    required: true,
+    bcrypt: true
+}
+})
+
+UserSchema.plugin(require('mongoose-bcrypt'));
+module.exports = mongoose.model("User", User); 
