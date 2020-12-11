8a2003c4cb61565b1a1ca4e35e8a6f27c630786d
katrinamarquez <katrina.l.marquez@gmail.com>
2020-12-11 11:09:29 +1000

    starting from auth fresh again

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
-const express = require("express")
-const expressSession = require('express-session')
+const express = require("express");
+const exphbs  = require('express-handlebars');
+const expressSession = require('express-session');
 const mongoose = require("mongoose")
 const cors = require("cors")
 const bodyParser = require("body-parser")
-const exphbs  = require('express-handlebars'); 
-
 const plantRouter = require("./routes/plant_routes.js")
-const authRouter = require("./routes/auth_routes");
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(cors());
+
 app.use(bodyParser.json());
-app.use(expressSession({
-  secret: 'treesarefriends',
-  resave: false, 
-  saveUninitialized: true,
-  cookie: { expires: 600000 }
-}))
 
 // when we need to deploy:
 if(process.env.NODE_ENV !== "production"){
@@ -45,8 +38,6 @@ mongoose.connect(
 )
 
 app.use("/plants", plantRouter)
-
-app.use("/users", userRouter)
 
 console.log("port: ",process.env.PORT)
 console.log(process.env)
