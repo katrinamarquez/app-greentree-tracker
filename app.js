b545b776a2a27aab548b4452f114b6729de6c4a2
katrinamarquez <katrina.l.marquez@gmail.com>
2020-12-14 09:39:56 +1000

    update so far

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs  = require('express-handlebars');
 const expressSession = require('express-session');
+const MongoStore = require('connect-mongo')(expressSession);
 const mongoose = require("mongoose")
 const cors = require("cors")
 const bodyParser = require("body-parser")
@@ -16,7 +17,19 @@ app.use(cors());
 app.use("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
+app.use(expressSession({
+  secret: 'treesarefriends',
+  resave: false, 
+  saveUninitialized: true,
+  cookie: { expires: 600000 },
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+})) 
+
 app.use(bodyParser.json());
+
+require("./config/passport");
+app.use(passport.initialize());
+app.use(passport.session());
 
 // when we need to deploy:
 if(process.env.NODE_ENV !== "production"){
