d9118b21cfd1b65d523338e244c87e2d0bfe8876
Tyler Hall <tylerhall12@outlook.com>
2020-12-15 09:54:21 +1000

    Adjusted schema

--- models/user.js
+++ models/user.js
@@ -3,16 +3,8 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose')
 
 const User = new Schema({
-    username: {
-        type: String, 
-        required: true
-    },
     email: {
         type: String,
-        required: true
-    },
-    password: {
-        type: String, 
         required: true
     },
     role: {
