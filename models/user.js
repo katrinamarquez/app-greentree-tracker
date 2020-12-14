90694d2ed36c81ec1d03bdc9d6577faad398b353
katrinamarquez <katrina.l.marquez@gmail.com>
2020-12-14 16:19:39 +1000

    add role on user schema

--- models/user.js
+++ models/user.js
@@ -14,6 +14,11 @@ const User = new Schema({
     password: {
         type: String, 
         required: true
+    },
+    role: {
+        type: String,
+        enum: ['business','customer'],
+        //required: true
     }
 });
 
