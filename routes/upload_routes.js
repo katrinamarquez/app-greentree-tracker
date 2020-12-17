16ea778790941e07c8eb0cbdfe7683c44f565f9d
katrinamarquez <katrina.l.marquez@gmail.com>
2020-12-17 11:11:50 +1000

    clean slate removed prev dependency

--- routes/upload_routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const fileUpload = require('express-fileupload');
-const router = express.Router()
-
-// upload endpoints
-router.post('/upload', (req, res) => {
-  if r
-})
