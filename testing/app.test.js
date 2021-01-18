724b89dd3aa957af1a29ae07e3143a68748facb9
katrinamarquez <katrina.l.marquez@gmail.com>
2021-01-19 09:54:02 +1000

    remove silly tests

--- testing/app.test.js
+++ testing/app.test.js
@@ -3,23 +3,9 @@ const request = require('supertest');
 
 var { app } = require('../app');
 
-describe('Landing page route exists.', () => {
-  it("Landing page working.", async () => {
-    const res = await request(app).get('/');
-    expectExport(res.statusCode).toEqual(200);
-  });
-});
-
 describe('Plants page route exists.', () => {
   it("Plants page working.", async () => {
     const res = await request(app).get('/plants');
-    expectExport(res.statusCode).toEqual(200);
-  });
-});
-
-describe('Contact us page route exists.', () => {
-  it("Contact us page working.", async () => {
-    const res = await request(app).get('/contact');
     expectExport(res.statusCode).toEqual(200);
   });
 });
