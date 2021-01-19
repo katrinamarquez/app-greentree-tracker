d8113f86f591a368d22c73600c6387a3e79f6fe7
katrinamarquez <katrina.l.marquez@gmail.com>
2021-01-19 15:10:48 +1000

    tests passing

--- testing/app.test.js
+++ testing/app.test.js
@@ -1,25 +1,74 @@
-const expectExport = require('expect');
 const request = require('supertest');
-
 var { app } = require('../app');
 
-describe('Plants page route exists.', () => {
-  it("Plants page working.", async () => {
-    const res = await request(app).get('/plants');
-    expectExport(res.statusCode).toEqual(200);
-  });
+// Test that you can get plants
+describe("Plants route test", () => {
+  it('plants route working yeet', async () => {
+    const res = await request(app)
+    .get('/plants')
+  expect(res.statusCode).toEqual(200)
+  })
+})
+
+// Test logging in as a Admin
+describe("User logging in test", () => {
+  it('user logged in', async () => {
+    request(app)
+      .post('/auth/login')
+      .set({
+        username: 'Admin',
+        password: 'Password123'
+      })
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+      })
+  })
 });
 
-describe('Login page route exists.', () => {
-  it("Login page working.", async () => {
-    const res = await request(app).get('/auth/login');
-    expectExport(res.statusCode).toEqual(200);
-  });
+// Test creating plants
+describe("Creating a plant as admin user", () => {
+  it('creating plant as admin user', async () => {
+    request(app)
+      .post('/plants/new')
+      .set({
+        botanical_name: 'planta testa',
+        price: 60,
+        pot_size: '350mm',
+        category: 'ground cover'
+      })
+      .then((response) => {
+        expect(response.statusCode).toBe(201);
+      })
+  })
 });
 
-describe('Register page route exists.', () => {
-  it("Register page working.", async () => {
-    const res = await request(app).get('/auth/register');
-    expectExport(res.statusCode).toEqual(200);
-  });
+// Test that you can login as a user (not an admin)
+describe("User logging in test", () => {
+  it('user logged in', async () => {
+    request(app)
+      .post('/auth/login')
+      .set({
+        username: 'Katrina Test',
+        password: 'password'
+      })
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+      })
+  })
+});
+
+// Test that you can register as a user
+describe("User registering account test", () => {
+  it('user registered', async () => {
+    request(app)
+      .post('/auth/register')
+      .set({
+        username: 'Imma User',
+        email: 'email@email.com',
+        password: 'password'
+      })
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+      })
+  })
 });
